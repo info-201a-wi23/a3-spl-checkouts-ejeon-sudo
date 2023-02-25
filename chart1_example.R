9e25a4a6d17703f7ffde576d43abbfcfe3fe489b
ejeon-sudo <elishayjeon@gmail.com>
2023-02-24 16:42:58 -0800

    fixed wrong code and rewrote chart 1

--- chart1_example.R
+++ chart1_example.R
@@ -11,21 +11,18 @@ most_popular_flyguys <- flyguy_df %>%
   pull(Title)
 
 # Get the five books' in one data frame
-my_popular_books <- flyguy_df %>%
-  filter(Title == most_popular_flyguys) %>%
-  select(CheckoutYear, Title, Checkouts) %>%
-  group_by(CheckoutYear, Title) %>% 
-  summarize(total_checkouts = sum(Checkouts))
-
+my_popular_books <- each_flyguy_yearly_checkout %>%
+  filter(Title %in% (most_popular_flyguys)) 
 
 # Plot a chart that shows this
 five_flyguys_checkouts <- ggplot(my_popular_books) + geom_line(aes(
   x = CheckoutYear, 
-  y = total_checkouts, 
+  y = each_total_checkout, 
   color = Title)) + 
   scale_color_brewer(palette = "Set2") +
-  labs(title = "Flyswatter Escapes Series' Doom Entering The 2020's", 
+  labs(title = "Fly Guy Series' Collective Doom Entering The 2020's", 
        x = "Year", 
        y = "Number of Checkouts",
        color = "Top 5 Most Popular Fly Guy Books") +
-  scale_y_continuous(breaks = seq(0, 260, 20))
+  scale_y_continuous(breaks = seq(0, 840, 100)) +
+  scale_x_continuous(breaks = seq(2017, 2023))
